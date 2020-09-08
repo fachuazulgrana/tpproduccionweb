255e759c0b7ecd5ea9410a6bb8c1a95b5bfcaa24
mateoporcar <37422714+mateoporcar@users.noreply.github.com>
2020-09-07 21:02:12 -0300

    Update productoportada.php

--- includes/productoportada.php
+++ includes/productoportada.php
@@ -7,31 +7,35 @@
         <?php
         $continente = (isset($_GET["continente"]) ? $_GET['continente'] : null);
         $pais = (isset($_GET["pais"]) ? $_GET['pais'] : null);
-        
+        $ciudad = (isset($_GET["ciudad"]) ? $_GET['ciudad'] : null);
 
-        foreach ($productos as $key => $value) { 
 
-          if ($page == 'index' && $value['destacado']) { 
+        foreach ($productos as $key => $value) {
 
-             include('card_paises.php'); 
+          if ($page == 'index' && $value['destacado']) {
 
-          } elseif ($page == 'catalogo') { 
+            include('card_paises.php');
+          } elseif ($page == 'catalogo') {
 
-            
+
             if (
-              ((empty($continente) || $continente == 'Todo') && empty($pais)) || // No se aplica filtro 
-              (empty($pais) && $continente == $value['continente']) || // Se filtra por continente
-              ((empty($continente) || $continente == 'Todo') && $pais == $value['nombre']) || // Se filtra por paises
-              ($pais == $value['nombre'] && $continente == $value['continente']) // Se filtra por continente y pais
-            ) 
-            {
+              ((empty($continente) || $continente == 'Todo') && empty($pais) && empty($ciudad) || // No se aplica filtro 
+              (empty($ciudad) && empty($pais) && $continente == $value['continente']) || // Se filtra por continente
+              ((empty($continente) || $continente == 'Todo') && empty($ciudad) && $pais == $value['pais'] ) || // Se filtra por paises
+              ((empty($continente) || $continente == 'Todo') && (empty($pais) || $pais == 'Todo') && $ciudad == $value['nombre'] ) ||
+              (empty($ciudad) && $pais == $value['pais'] && $continente == $value['continente']) ||
+              ($continente == $value['continente'] && $pais == $value['pais'] && $ciudad == $value['nombre']) // Se filtra por continente y pais
+              ) 
             
+            )
+                
+            {
+
 
               include('card_paises.php');
-
-            } 
+            }
           }
-        } 
+        }
         ?>
 
       </div>
