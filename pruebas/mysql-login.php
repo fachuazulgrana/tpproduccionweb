7834b2e35dabca014fcce3b7e070925e50cdc896
mateoporcar <mateo.porcar@gmail.com>
2020-09-15 18:10:16 -0300

    rutas genericas

--- pruebas/mysql-login.php
+++ pruebas/mysql-login.php
@@ -8,4 +8,14 @@
     /*
     define('BASEURL','127.0.0.1/grupo1/');
     */
+
+define("SERVIDOR", "http://localhost/produccion_web");
+define("RUTA_HOME", SERVIDOR . "/index.php");
+define("RUTA_CATALOGO", SERVIDOR . "/catalogo.php");
+define("RUTA_PAQUETES", SERVIDOR . "/paquetes.php");
+define("RUTA_CONTACTO", SERVIDOR . "/contacto.php");
+
+define("RUTA_CSS", SERVIDOR . "/CSS/");
+define("RUTA_JS", SERVIDOR . "/js/");
+
 ?>
