b493ff001310eb2d58fca60d3e8f91b5c0add340
mateoporcar <37422714+mateoporcar@users.noreply.github.com>
2020-09-15 01:33:04 -0300

    Update card_paises.php

--- includes/card_paises.php
+++ includes/card_paises.php
@@ -2,23 +2,22 @@
               <div class="card carta">
 
               <?php 
-                //foreach ($Productos->getProductos() as $ciudades)
-                echo '<img src="' .  $value["url"] . '" class="card-img-top" alt="..." />' 
+                echo '<img src="' .  "URL A DEFINIR" . '" class="card-img-top" alt="..." />' 
               ?>
 
                 <div class="card-body">
-                  <h5 class="card-title1 font-weight-bold"><?php echo $value["nombre"]; ?></h5>
-                  <p class="card-text"><?php echo cortar($value["descripcion"]); ?></p>
+                  <h5 class="card-title1 font-weight-bold"><?php echo $prod_final->obtener_ciudad(); ?></h5>
+                  <p class="card-text"><?php echo cortar($prod_final->obtener_descripcion()); ?></p>
                   <div class="row justify-content-center pt-1 pb-3">
                     <h5>
-                      <span class="card-text text-center badge badge-light"><?php echo $value["precio"]; ?></span>
+                      <span class="card-text text-center badge badge-light"><?php echo $prod_final->obtener_precio(); ?></span>
                     </h5>
                   </div>
                   <div class="container d-flex justify-content-around">
                     <a href="#" class="btn btn-success">Comprar</a>
 
                     
-                    <?php echo '<a href="product_details.php?id=' . $value["id"] . '"class="btn btn-outline-primary">Ver mas</a>' ?>
+                    <?php echo '<a href="product_details.php?id=' . $prod_final->obtener_id() . '"class="btn btn-outline-primary">Ver mas</a>' ?>
                   </div>
                 </div>
               </div>
