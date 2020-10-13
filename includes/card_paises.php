61fecb31a1e84b65e030b898f1ec449d1b7c78ae
mcostilla <mat.cost@gmail.com>
2020-10-13 00:44:18 -0300

    Estilo para distinguir destacados

--- includes/card_paises.php
+++ includes/card_paises.php
@@ -1,10 +1,23 @@
             <div class="col-sm-12 col-md-12 col-lg-6 col-xl-4 my-3">
-              <div class="card carta">
+              <div class="card carta <?php 
+              
+              if($ciudades['destacado']==1){
+                echo "border border-warning";}
+              else {
+                echo "";
+              } ?>">
 
               <img height="100%" width="100%" src="pruebas/img/<?php echo $ciudades['id'] ?>.jpg" alt="imagen de <?php echo $ciudades['nombre']; ?>">
 
                 <div class="card-body">
-                  <h5 class="card-title1 font-weight-bold"><?php echo $ciudades["nombre"]; ?></h5>
+                  <h5 class="card-title1 font-weight-bold <?php 
+              
+              if($ciudades['destacado']==1){
+                echo "text-warning";}
+              else {
+                echo "";
+              } ?>
+                  "><?php echo $ciudades["nombre"]; ?></h5>
                   <p class="card-text"><?php echo cortar($ciudades["descripcion"]); ?></p>
                   <div class="row justify-content-center pt-1 pb-3">
                     <h5>
@@ -14,7 +27,13 @@
                     </h5>
                   </div>
                   <div class="container d-flex justify-content-around">
-                    <a href="#" class="btn btn-success">Comprar</a>
+                    <a href="#" class="btn <?php 
+              
+              if($ciudades['destacado']==1){
+                echo "btn-warning";}
+              else {
+                echo "btn-success";
+              } ?>">Comprar</a>
 
                     
                     <?php echo '<a href="product_details.php?id=' . $ciudades["id"] . '"class="btn btn-outline-primary">Ver mas</a>' ?>
