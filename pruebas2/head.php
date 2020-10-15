c61b7604acaa21b2430d8cd744eac326a02698a9
Alberto Bossio <alberto.federico.bossio@gmail.com>
2020-10-14 23:00:29 -0300

    la base del backend

---
+++ pruebas2/head.php
@@ -0,0 +1,19 @@
+<!-- 
+    ACÁ VA LA NUEVA BASE DE DATOS (o podemos usar la misma)
+    <?php //require_once('pruebas/mysql-login.php'); ?> 
+-->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Styles -->
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="<?php echo RUTA_CSS ?>bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo RUTA_CSS ?>jquery.fancybox.min.css" />
+<script src="<?php echo RUTA_JS ?>jquery-3.5.1.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!--
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+-->
---
+++ pruebas2/perfiles.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php require_once "head.php" ?>
+    <title>BackEnd</title>
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand">
+    <img src="../images/logo_cabecera.png" alt="logo" style="width:40px;">
+   </a>
+   <a class="navbar-brand asd">Bienvenido al Panel BackEnd</a>
+</nav>
+
+
+<div class="sidebar">
+  <a href="home.php">Home</a>
+  <a href="productos.php">Productos</a>
+  <a href="promociones.php">Promociones</a>
+  <a href="noticias.php">Noticias</a>
+  <a href="usuarios.php">Usuarios</a>
+  <a class="active">Perfiles</a>
+  <a href="logout.php">Logout</a>
+  <a href="export.php">Export</a>
+</div>
+
+<div class="content">
+  <h2>Responsive Sidebar Example</h2>
+  <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
+  <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
+ 
+</div>
+
+</body>
+</html>
