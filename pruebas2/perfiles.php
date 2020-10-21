9613fefe81283fd026c2629304189cb892af4d48
mateoporcar <37422714+mateoporcar@users.noreply.github.com>
2020-10-21 00:20:24 -0300

    Update perfiles.php

--- pruebas2/perfiles.php
+++ pruebas2/perfiles.php
@@ -2,23 +2,78 @@
 <html lang="es">
 
 <head>
-    <?php require_once "head.php" ?>
-    <title>Perfiles</title>
+  <?php
+  require_once "head_admin.php";
+  require_once('../app/Perfil.php');
+  require_once('../pruebas/mysql-login.php');
+
+  try{
+    $con = new PDO('mysql:host='.$hostname.';dbname='.$database.';port='.$puerto, $username, $password);
+  }catch (PDOException $e){
+    print "¡Error!: " . $e->getMessage();
+    die();
+  }
+
+
+  $Perfil = new Perfil($con);
+  $page = "perfiles";
+  ?>
+  <title>Perfiles</title>
+
 </head>
 
-<body>
+<?php require_once "sidebar.php"; ?>
 
-<?php 
-$page = 'perfiles';
-require_once "sidebar.php" 
+<?php
+if (isset($_POST['formulario-perfiles'])) {
+  if ($_POST['id_perfil'] > 0) {
+    $Perfil->edit($_POST);
+  } else {
+    $Perfil->save($_POST);
+  }
+  header('Location: perfiles.php');
+}
+
+if (isset($_GET['del'])) {
+  $resp = $Perfil->del($_GET['del']);
+  if ($resp == 1) {
+    header('Location: perfiles.php');
+  }
+  echo '<script>alert("' . $resp . '");</script>';
+}
 ?>
 
-<div class="content">
-  <h2>Responsive Sidebar Example</h2>
-  <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
-  <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
- 
-</div>
+<body>
+  <div class="content">
+    <h1 class="page-header">Perfiles</h1>
+    <h2 class="sub-header">Listado <a href="perfiles_ae.php"><button type="button">AGREGAR</button></a></h2>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          foreach ($Perfil->getList() as $perfil) { ?>
+            <tr>
+              <td><?php echo $perfil['id_perfil']; ?></td>
+              <td><?php echo $perfil['nombre']; ?></td>
+              <td>
+                <a href="perfiles_ae.php?edit=<?php echo $perfil['id_perfil'] ?>"><button type="button">M</button></a>
+                <a href="perfiles.php?del=<?php echo $perfil['id_perfil'] ?>"><button type="button">X</button></a>
+              </td>
+            </tr>
 
+          <?php
+          }
+
+          ?>
+        </tbody>
+      </table>
 </body>
+
 </html>
